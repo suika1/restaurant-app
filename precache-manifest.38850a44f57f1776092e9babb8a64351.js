5c74d08b3aac90ab0da0ea5560cbf265b2d908cd
suika1 <vanyoktank1@mail.ru>
2019-05-14 22:19:39 +0300

    Updates

---
+++ precache-manifest.38850a44f57f1776092e9babb8a64351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70f58994863bed47aa88",
+    "url": "/restaurant-app/static/js/runtime~main.70f58994.js"
+  },
+  {
+    "revision": "bdf06cb1766175766f33",
+    "url": "/restaurant-app/static/js/main.bdf06cb1.chunk.js"
+  },
+  {
+    "revision": "81e1c779ee96def26bf2",
+    "url": "/restaurant-app/static/js/1.81e1c779.chunk.js"
+  },
+  {
+    "revision": "bdf06cb1766175766f33",
+    "url": "/restaurant-app/static/css/main.074c4ba6.chunk.css"
+  },
+  {
+    "revision": "084c245f33487963b71c620d57236361",
+    "url": "/restaurant-app/index.html"
+  }
+];
